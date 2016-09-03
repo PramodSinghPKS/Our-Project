a434048b35302c7922208cba0e8d0b44a61fe6d1
Pramod Kumar Singh <psingh.190.ps@gmail.com>
2016-09-03 08:27:46 +0530

    Doctype to HTML5 style

--- Our Project/index.php
+++ Our Project/index.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-	
 		<meta charset="utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<meta name="author" content="Rahul Kumar"/>
